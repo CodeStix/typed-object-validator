7d413cb226b8c4bfa5b992714d1c1e7b636304e5
Stijn Rogiest <reddusted@gmail.com>
2021-04-01 16:36:03 +0200

    Seperate null schema

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ export function test() {
 }
 
 interface Person {
-    firstName?: "nice" | number | boolean;
-    lastName: string | null;
-    email?: "0" | null;
+    firstName?: number | boolean;
+    lastName?: string;
+    email?: string;
 }
 
 type IsUnion<T, U, V> = string | number extends T
@@ -30,30 +30,36 @@ type IsUnion<T, U, V> = string | number extends T
     ? U
     : V;
 
+type NullableSchema<T> = undefined extends T
+    ? { undefined: true } & NullableSchema<T extends undefined ? never : T>
+    : null extends T
+    ? { null: true } & NullableSchema<T extends null ? never : T>
+    : ValueSchema<T> extends [any, any, ...any]
+    ? { or: ValueSchema<T> }
+    : ValueSchema<T>[any];
+
 type ValueSchema<T> = undefined extends T
     ? [{ undefined: true }, ...ValueSchema<T extends undefined ? never : T>]
     : null extends T
     ? [{ null: true }, ...ValueSchema<T extends null ? never : T>]
     : number extends T
     ? [{ type: "number" }, ...ValueSchema<T extends number ? never : T>]
-    : string extends T
-    ? [{ type: "string" }, ...ValueSchema<T extends string ? never : T>]
     : boolean extends T
     ? [{ type: "boolean" }, ...ValueSchema<T extends boolean ? never : T>]
-    : [];
+    : string extends T
+    ? [{ type: "string" }, ...ValueSchema<T extends string ? never : T>]
+    : // : T extends string
+      // ? [{ type: "string"; value2: T }]
+      [];
 
 type ObjectSchema<T extends {}> = {
-    [Key in keyof T]-?: ValueSchema<T[Key]>;
+    [Key in keyof T]-?: NullableSchema<T[Key]>;
 };
 
-type A = string | number extends string ? true : false;
-
-let a: IsUnion<boolean | number, true, false>;
-
 const PersonSchema = createSchema<Person>({
-    firstName: [{ undefined: true }, { type: "number" }, { type: "boolean" }],
-    lastName: [{ null: true }, { type: "string" }],
-    email: [{ undefined: true }, { null: true }],
+    firstName: { undefined: true, or: [{ type: "number" }, { type: "boolean" }] },
+    lastName: { undefined: true, type: "string" },
+    email: { type: "string", undefined: true },
 });
 
 function createSchema<T>(schema: ObjectSchema<T>) {}
