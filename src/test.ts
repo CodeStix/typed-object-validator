84ece3ac85dca7a26fb1e4ba38e820ff6abe9044
Stijn Rogiest <reddusted@gmail.com>
2021-04-03 22:33:08 +0200

    LenghtySchema

--- src/test.ts
+++ src/test.ts
@@ -2,9 +2,13 @@ function string(): StringSchema {
     return new StringSchema();
 }
 
-class Schema<T> {
-    private isNullable = false;
-    private isOptional = false;
+export type ErrorMap<T> = {
+    [Key in keyof T]?: NonNullable<T[Key]> extends {} ? ErrorMap<NonNullable<T[Key]>> : string;
+};
+
+export abstract class Schema<T> {
+    protected isNullable = false;
+    protected isOptional = false;
 
     optional(): Schema<T | undefined> {
         this.isOptional = true;
@@ -19,20 +23,36 @@ class Schema<T> {
     or<D>(other: Schema<D>): Schema<SchemaType<Schema<T> | Schema<D>>> {
         return new OrSchema([this, other]);
     }
+
+    public abstract validate(value: T): ErrorMap<T>;
+    public abstract clean(value: T): T;
 }
 
-class StringSchema extends Schema<string> {
-    private minLength?: number;
-    private maxLength?: number;
+abstract class LengthySchema<T> extends Schema<T> {
+    protected minLength?: number;
+    protected minLengthMessage?: string;
+    protected maxLength?: number;
+    protected maxLengthMessage?: string;
 
-    min(min: number): StringSchema {
+    public min(min: number, message?: string) {
         this.minLength = min;
+        this.minLengthMessage = message;
         return this;
     }
 
-    max(max: number): StringSchema {
+    public max(max: number, message?: string) {
         this.maxLength = max;
+        this.maxLengthMessage = message;
         return this;
+    }
+}
+
+class StringSchema extends LengthySchema<string> {
+    public validate(value: string): string {
+        if (typeof value !== "string") return;
+    }
+    public clean(value: string): string {
+        throw new Error("Method not implemented.");
     }
 }
 
@@ -50,10 +70,6 @@ function object<T>(fields: ObjectFieldSchemas<T>) {
     return new ObjectSchema(fields);
 }
 
-type OrFieldSchemas<T> = {
-    [Key in keyof T]: Schema<T[Key]>;
-};
-
 type SchemaType<T extends Schema<any>> = T extends Schema<infer D> ? D : never;
 
 class OrSchema<T extends [Schema<any>, ...Schema<any>[]]> extends Schema<SchemaType<T[number]>> {
@@ -66,6 +82,28 @@ function or<T extends [Schema<any>, ...Schema<any>[]]>(schemas: T): Schema<Schem
     return new OrSchema(schemas);
 }
 
+class ArraySchema<T> extends LengthySchema<T[]> {
+    constructor(private schema: Schema<T>) {
+        super();
+    }
+}
+
+function array<T>(schema: Schema<T>) {
+    return new ArraySchema(schema);
+}
+
+// class TupleSchema<T extends [Schema<T>, ...Schema<T>[]]> extends Schema<SchemaType<T[number]>> {
+
+//     constructor(schemas: T) {
+//         super()
+//     }
+
+// }
+
+// function tuple<T extends [Schema<T>, ...Schema<T>[]]>(schemas: T): Schema<Ex<T>> {
+//     return new TupleSchema(schemas);
+// }
+
 let a = string().min(100).max(21);
 let d = string().optional();
 
@@ -75,3 +113,8 @@ let b = object({
 });
 
 let c = string().or(object({ yikes: string() }));
+
+let f = array(string()).or(array(string()));
+let g = array(string()).min(100).or(string());
+
+// let g = tuple([string(), string()])
